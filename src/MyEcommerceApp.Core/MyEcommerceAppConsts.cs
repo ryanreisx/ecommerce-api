7103d906b8132562e309ff255aaa9b05e10495b2
Ryan <ryanreis280903@gmail.com>
2023-08-06 21:48:26 -0300

    first commit

---
+++ src/MyEcommerceApp.Core/MyEcommerceAppConsts.cs
@@ -0,0 +1,20 @@
+﻿using MyEcommerceApp.Debugging;
+
+namespace MyEcommerceApp
+{
+    public class MyEcommerceAppConsts
+    {
+        public const string LocalizationSourceName = "MyEcommerceApp";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "2fe282725dd04079917dae42d246721f";
+    }
+}
